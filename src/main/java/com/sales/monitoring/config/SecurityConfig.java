730109716343f0aacfc0c12e6ce31f3c52d491bb
Gheyson Melo <gheysonmelo@hotmail.com>
2025-04-04 00:32:37 -0300

    add: corsConfig to integrate with front

--- src/main/java/com/sales/monitoring/config/SecurityConfig.java
+++ src/main/java/com/sales/monitoring/config/SecurityConfig.java
@@ -20,35 +20,30 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        // Desabilita CSRF para o H2 Console e para as rotas de usuário
-        http.csrf(csrf -> csrf
-            .ignoringRequestMatchers(
-                new AntPathRequestMatcher("/h2-console/**"),
-                new AntPathRequestMatcher("/user/**"),
-                new AntPathRequestMatcher("/auth/**"),
-                new AntPathRequestMatcher("/product/**"),
-                new AntPathRequestMatcher("/sale/**")
+        http
+            .cors() // Habilita a integração do Spring Security com a configuração global de CORS
+            .and()
+            .csrf(csrf -> csrf
+                .ignoringRequestMatchers(
+                    new AntPathRequestMatcher("/h2-console/**"),
+                    new AntPathRequestMatcher("/user/**"),
+                    new AntPathRequestMatcher("/auth/**"),
+                    new AntPathRequestMatcher("/product/**"),
+                    new AntPathRequestMatcher("/sale/**")
+                )
             )
-        );
-
-        // Configura as permissões das requisições
-        http.authorizeHttpRequests(auth -> auth
-            .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
-            .requestMatchers(new AntPathRequestMatcher("/user")).permitAll()
-            .requestMatchers(new AntPathRequestMatcher("/auth/**")).permitAll()
-            .anyRequest().authenticated()
-        );
-
-        // Permite que o H2 Console seja exibido em frames
-        http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
-
-        // Adiciona o filtro JWT antes do filtro padrão de autenticação
-        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/user")).permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/auth/**")).permitAll()
+                .anyRequest().authenticated()
+            )
+            .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()))
+            .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
 
-    // Registra o AuthenticationManager para que ele possa ser injetado no AuthService
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
          return authenticationConfiguration.getAuthenticationManager();
